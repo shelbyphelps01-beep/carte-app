6f46f9394fa7b10ed9fd63dc29b4ecc1be73cc3d
shelbyphelps01-beep <shelbyphelps01@gmail.com>
2026-02-13 13:07:22 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// Service Worker for Carte PWA
+const CACHE_NAME = 'carte-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch new
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
